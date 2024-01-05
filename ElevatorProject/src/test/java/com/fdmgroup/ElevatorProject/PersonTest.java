e733377b155dc3ff5a30d0b4bd777c3e08c566ba
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2024-01-05 17:21:53 +1100

    Added tests for Person object: check proper instantiation, check if goingUp() properly detected

--- ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/PersonTest.java
+++ ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/PersonTest.java
@@ -1,5 +1,33 @@
 package com.fdmgroup.ElevatorProject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class PersonTest {
+	
+	@Test
+	void check_person_instantation() {
+		Person person = new Person(0,1);
+		
+		assertEquals(0, person.getSrcFloor());
+		assertEquals(1, person.getDestFloor());
+	}
+	
+	@Test
+	void check_person_if_going_up() {
+		Person person = new Person(0,1);
+		
+		assertTrue(person.isGoingUp());
+	}
+	
+	@Test
+	void check_person_if_going_down() {
+		Person person = new Person(5,0);
+		
+		assertFalse(person.isGoingUp());
+	}
 
 }
