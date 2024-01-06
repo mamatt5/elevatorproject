3d38f9567dee9e2f014d7d6c7701a90d461e7398
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2024-01-07 02:50:34 +1100

    Some comments for next steps on what to do. handlePeopleWaiting method commented out for now.

--- ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/Controller.java
+++ ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/Controller.java
@@ -19,6 +19,7 @@ public class Controller {
 	}
 
 
+	// Might have to move this method to Person object
 	public Person StringToPerson(String input) {
 		String[] personString = input.split(":");
 		int srcFloor = Integer.parseInt(personString[0]);
@@ -38,6 +39,8 @@ public class Controller {
 		}
 	}
 	
+	
+	// For testing purposes
 	public Elevator FindElevatorWithPerson(Person person) {
 		for ( Elevator elevator : scheduler.getElevators() ) {
 			if ( elevator.getPeopleInside().contains(person) ) {
@@ -48,22 +51,22 @@ public class Controller {
 		return null; // if no person exists inside any of the elevators
 	}
 	
-	// This is to convert string input into People object, hold testing for now!
-	public void handlePeopleWaiting(String input) {
-		
-		// sample input: "1:4,12:3,5:13", where "src:dest" notation
-		
-		// input will be broken down to a peopleList: {1:4, 12:3, 5:13}
-		String[] peopleList = input.split(",");
-		
-		// for each "src:dest" string in peopleList
-		for ( int i = 0 ; i <= peopleList.length-1 ; i++ ) {
-			
-			// each element in personString is {src, dest}, then converted into Person object
-			Person person = StringToPerson(peopleList[i]);
-			peopleQueue.add(person);
-		}
-		
-	}
+//	// This is to convert string input into People object, hold testing for now!
+//	public void handlePeopleWaiting(String input) {
+//		
+//		// sample input: "1:4,12:3,5:13", where "src:dest" notation
+//		
+//		// input will be broken down to a peopleList: {1:4, 12:3, 5:13}
+//		String[] peopleList = input.split(",");
+//		
+//		// for each "src:dest" string in peopleList
+//		for ( int i = 0 ; i <= peopleList.length-1 ; i++ ) {
+//			
+//			// each element in personString is {src, dest}, then converted into Person object
+//			Person person = StringToPerson(peopleList[i]);
+//			peopleQueue.add(person);
+//		}
+//		
+//	}
 	
 }
