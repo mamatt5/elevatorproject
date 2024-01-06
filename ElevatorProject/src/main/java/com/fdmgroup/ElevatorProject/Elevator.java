3a1c77d976a882930489b154a07dd019d1cc3597
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2024-01-06 21:15:55 +1100

    Added tests to Scheduler to check if the scheduler can add an elevator, and can assign an elevator to a person

--- ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/Elevator.java
+++ ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/Elevator.java
@@ -7,8 +7,8 @@ public class Elevator implements Runnable {
 	private boolean isIdle = true;
 	private int currentFloor = 0;
 	private ArrayList<Person> peopleInside = new ArrayList<>();
-	private int minFloor;
-	private int maxFloor;
+	private int minFloor = 0;
+	private int maxFloor = 0;
 	
 	
 	// Getters and Setters
--- ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/SchedulerTest.java
+++ ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/SchedulerTest.java
@@ -1,5 +1,44 @@
 package com.fdmgroup.ElevatorProject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SchedulerTest {
+	Scheduler scheduler;
+	
+	@BeforeEach
+	void setup() {
+		Elevator elevator1 = new Elevator();
+		Elevator elevator2 = new Elevator();
+		Elevator elevator3 = new Elevator();
+		ArrayList<Elevator> elevators = new ArrayList<Elevator>();
+		
+		elevators.add(elevator1);
+		elevators.add(elevator2);
+		elevators.add(elevator3);
+		
+		scheduler = new Scheduler(elevators);
+	}
+	
+	@Test
+	void scheduler_can_add_elevator() {
+		Elevator elevator4 = new Elevator();
+		scheduler.addElevator(elevator4);
+		
+		assertEquals(4, scheduler.getElevators().size());
+	}
+	
+	@Test
+	void scheduler_can_assign_elevator_to_a_person() {
+		Person person = new Person(0,10);
+		Elevator assignedElevator = scheduler.callElevator(person);
+		
+		assertNotNull(assignedElevator);
+	}
 
 }
