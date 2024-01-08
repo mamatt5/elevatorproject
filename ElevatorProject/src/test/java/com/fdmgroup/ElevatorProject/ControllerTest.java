19f53523b479100ca5980b51b58b8ac72e00541b
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2024-01-08 15:13:41 +1100

    Testing Controller
    also added @SuppressWarnings for implementing serialising in some classes to avoid unnecessary warnings

--- ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/ControllerTest.java
+++ ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/ControllerTest.java
@@ -8,71 +8,242 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ControllerTest {
+public class ControllerTest
+{
 	Controller controller;
-	
+
 	@BeforeEach
-	void setup() {
+	void setup()
+	{
 		Elevator elevator1 = new Elevator();
 		Elevator elevator2 = new Elevator();
 		Elevator elevator3 = new Elevator();
 		ArrayList<Elevator> elevators = new ArrayList<Elevator>();
-		
+
 		elevators.add(elevator1);
 		elevators.add(elevator2);
 		elevators.add(elevator3);
 		Scheduler scheduler = new Scheduler(elevators);
-		
+
 		controller = new Controller(scheduler);
-		
+
 	}
-	
+
 	@Test
-	void convert_string_to_Person_object() {
-		Person person1 = new Person(0,10);
+	void assign_elevator_with_empty_queue() {
+		assertEquals(0, controller.getPeopleQueue().size());
 		
+		try { 
+			controller.assignElevator();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(0, controller.getPeopleQueue().size());
+	}
+
+	@Test
+	void convert_string_to_Person_object()
+	{
+		Person person1 = new Person(0, 10);
+
 		String stringPerson = "0:10";
 		Person person2 = controller.StringToPerson(stringPerson);
-		
+
 		assertEquals(person1.getSrcFloor(), person2.getSrcFloor());
 		assertEquals(person1.getDestFloor(), person2.getDestFloor());
 	}
-	
+
 	@Test
-	void add_person_to_queue() {
-		Person person1 = new Person(0,10);
-		
+	void add_person_to_queue_from_ground_floor_going_up()
+	{
+		Person person1 = new Person(0, 10);
+
 		controller.addPersonToQueue(person1);
-		
+
 		assertEquals(1, controller.getPeopleQueue().size());
 	}
 	
 	@Test
-	void add_multiple_person_to_queue() {
-		Person person1 = new Person(0,10);
-		Person person2 = new Person(0,5);
-		Person person3 = new Person(0,1);
-		
+	void add_person_to_queue_from_different_floor_going_up()
+	{
+		Person person1 = new Person(3, 10);
+
+		controller.addPersonToQueue(person1);
+
+		assertEquals(1, controller.getPeopleQueue().size());
+	}
+	
+	@Test
+	void add_person_to_queue_from_going_down_to_different_floor()
+	{
+		Person person1 = new Person(10, 3);
+
+		controller.addPersonToQueue(person1);
+
+		assertEquals(1, controller.getPeopleQueue().size());
+	}
+	
+	@Test
+	void add_person_to_queue_from_going_down_to_ground_floor()
+	{
+		Person person1 = new Person(6, 0);
+
+		controller.addPersonToQueue(person1);
+
+		assertEquals(1, controller.getPeopleQueue().size());
+	}
+
+	@Test
+	void add_multiple_person_to_queue_from_ground_floor()
+	{
+		Person person1 = new Person(0, 10);
+		Person person2 = new Person(0, 5);
+		Person person3 = new Person(0, 1);
+
 		controller.addPersonToQueue(person1);
 		controller.addPersonToQueue(person2);
 		controller.addPersonToQueue(person3);
-		
+
 		assertEquals(3, controller.getPeopleQueue().size());
 	}
 	
 	@Test
-	void assign_elevator_through_controller() {
-		Person person1 = new Person(0,1);
-		
+	void add_multiple_person_to_queue_from_different_floors_going_up()
+	{
+		Person person1 = new Person(3, 10);
+		Person person2 = new Person(2, 5);
+		Person person3 = new Person(0, 1);
+
 		controller.addPersonToQueue(person1);
-		try {
+		controller.addPersonToQueue(person2);
+		controller.addPersonToQueue(person3);
+
+		assertEquals(3, controller.getPeopleQueue().size());
+	}
+	
+	@Test
+	void add_multiple_person_to_queue_from_different_floors_going_down()
+	{
+		Person person1 = new Person(10, 2);
+		Person person2 = new Person(7, 1);
+		Person person3 = new Person(4, 3);
+
+		controller.addPersonToQueue(person1);
+		controller.addPersonToQueue(person2);
+		controller.addPersonToQueue(person3);
+
+		assertEquals(3, controller.getPeopleQueue().size());
+	}
+
+	@Test
+	void assign_elevator_with_1_person_ground_floor_going_up()
+	{
+		Person person1 = new Person(0, 1);
+
+		controller.addPersonToQueue(person1);
+		try
+		{
 			controller.assignElevator();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		} catch (InterruptedException e)
+		{
 			e.printStackTrace();
 		}
-		
+
 		assertTrue(controller.FindElevatorWithPerson(person1).getPeopleInside().contains(person1));
 	}
+	
+	@Test
+	void assign_elevator_3_person_ground_floor_going_up()
+	{
+		Person person1 = new Person(0, 10);
+		Person person2 = new Person(0, 5);
+		Person person3 = new Person(0, 1);
+
+		controller.addPersonToQueue(person1);
+		controller.addPersonToQueue(person2);
+		controller.addPersonToQueue(person3);
+		try
+		{
+			controller.assignElevator();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		assertTrue(controller.FindElevatorWithPerson(person1).getPeopleInside().contains(person1));
+		assertTrue(controller.FindElevatorWithPerson(person2).getPeopleInside().contains(person2));
+		assertTrue(controller.FindElevatorWithPerson(person3).getPeopleInside().contains(person3));
+	}
+	
+	@Test
+	void assign_elevator_3_person_different_floor_going_up()
+	{
+		Person person1 = new Person(3, 10);
+		Person person2 = new Person(2, 5);
+		Person person3 = new Person(6, 8);
+
+		controller.addPersonToQueue(person1);
+		controller.addPersonToQueue(person2);
+		controller.addPersonToQueue(person3);
+		try
+		{
+			controller.assignElevator();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		assertTrue(controller.FindElevatorWithPerson(person1).getPeopleInside().contains(person1));
+		assertTrue(controller.FindElevatorWithPerson(person2).getPeopleInside().contains(person2));
+		assertTrue(controller.FindElevatorWithPerson(person3).getPeopleInside().contains(person3));
+	}
+	
+	@Test
+	void assign_elevator_1_person_going_down_ground_floor()
+	{
+		Person person1 = new Person(3, 0);
+		Person person2 = new Person(2, 0);
+		Person person3 = new Person(6, 0);
+
+		controller.addPersonToQueue(person1);
+		controller.addPersonToQueue(person2);
+		controller.addPersonToQueue(person3);
+		try
+		{
+			controller.assignElevator();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		assertTrue(controller.FindElevatorWithPerson(person1).getPeopleInside().contains(person1));
+		assertTrue(controller.FindElevatorWithPerson(person2).getPeopleInside().contains(person2));
+		assertTrue(controller.FindElevatorWithPerson(person3).getPeopleInside().contains(person3));
+	}
+	
+	@Test
+	void assign_elevator_1_person_going_down_different_floor()
+	{
+		Person person1 = new Person(3, 1);
+		Person person2 = new Person(7, 2);
+		Person person3 = new Person(6, 3);
+
+		controller.addPersonToQueue(person1);
+		controller.addPersonToQueue(person2);
+		controller.addPersonToQueue(person3);
+		try
+		{
+			controller.assignElevator();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		assertTrue(controller.FindElevatorWithPerson(person1).getPeopleInside().contains(person1));
+		assertTrue(controller.FindElevatorWithPerson(person2).getPeopleInside().contains(person2));
+		assertTrue(controller.FindElevatorWithPerson(person3).getPeopleInside().contains(person3));
+	}
+	
 
 }
