e25a88005c42a0584b8d3ec296c63b2845efc5e6
Luna <lucina.fan@students.mq.edu.au>
2024-01-08 15:05:35 +1100

    added a split for different people based on comma (,) delimiter

--- ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/InputValidation.java
+++ ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/InputValidation.java
@@ -6,27 +6,27 @@ public class InputValidation {
 
     // fixme temporary min and max values
     // todo @Jefferson minFloor and maxFloor should be from config (?)
-    private int minFloor = 0;
-    private int maxFloor = 10;
+    private final int MIN_FLOOR = 0;
+    private final int MAX_FLOOR = 10;
 
     // getters
     public int getMinFloor() {
-        return minFloor;
+        return MIN_FLOOR;
     }
 
     public int getMaxFloor() {
-        return maxFloor;
+        return MAX_FLOOR;
     }
 
 
     // methods
 
     public boolean isValidFloor(int floor) {
-        return floor >= minFloor && floor <= maxFloor;
+        return floor >= MIN_FLOOR && floor <= MAX_FLOOR;
     }
 
-    // expected format is "src:dest"
-    public boolean isValidInput(@NotNull String input) {
+    // expected floor-to-floor request format is "src:dest"
+    public boolean isValidRequest(@NotNull String input) {
         input = input.replaceAll(" ", "");
 
         String[] floors = input.split(":");		// two and only two parts:
@@ -50,5 +50,19 @@ public class InputValidation {
             // todo @Jasper to log error
             return false;
         }
+    }
+
+    // expected input format is "src:dest,src:dest,src:dest..."
+    public boolean isValidInput(@NotNull String input) {
+        String[] requests = input.split(",");
+
+        for (String request : requests) {
+
+            if (!isValidRequest(request)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
