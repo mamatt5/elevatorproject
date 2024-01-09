e315d6bc427b4410ee85307da42ef8c503e41ccc
SillyCiely <sillyciely@gmail.com>
2024-01-09 17:06:29 +1100

    TDD testing for refactoring main inputValidation method

--- ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/InvalidInputException.java
+++ ElevatorProject/src/main/java/com/fdmgroup/ElevatorProject/InvalidInputException.java
@@ -12,9 +12,15 @@ public class InvalidInputException extends Exception {
     }
 
     // invalid floor inputs throw InvalidInputException
-    public void validateFloor(int floor) throws InvalidInputException {
-        if (!validation.isValidFloor(floor)) {
-            throw new InvalidInputException("Input floor value `" + floor + "` is invalid");
+    public void validateTrip(int src, int dest) throws InvalidInputException {
+        if (!validation.floorInRange(src)) {
+            throw new InvalidInputException("Floor out of range-- " + src);
+        }
+        if (!validation.floorInRange(dest)) {
+            throw new InvalidInputException("Floor out of range-- " + dest);
+        }
+        if (!validation.sameFloor(src, dest)) {
+            throw new InvalidInputException("No travel required-- " + src + ", " + dest);
         }
     }
 
