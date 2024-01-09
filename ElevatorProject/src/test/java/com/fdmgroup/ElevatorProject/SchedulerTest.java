c88e31bd2ff18cbf3ed7d65222b1fb187f16e0d9
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2024-01-09 11:03:59 +1100

    Revert "Updated Test"
    This reverts commit cfd629fda142a9b1b46773713881d3a3e65ddf4c.

--- ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/SchedulerTest.java
+++ ElevatorProject/src/test/java/com/fdmgroup/ElevatorProject/SchedulerTest.java
@@ -1,9 +1,7 @@
 package com.fdmgroup.ElevatorProject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
@@ -42,7 +40,6 @@ public class SchedulerTest {
 		Elevator assignedElevator = scheduler.CallElevator(person);
 
 		assertNotNull(assignedElevator);
-		assertTrue(scheduler.getElevators().contains(assignedElevator));
 	}
 	
 	@Test
@@ -51,7 +48,7 @@ public class SchedulerTest {
 		Person person2 = new Person(0,10);
 
 		Elevator assignedElevator1 = scheduler.CallElevator(person1);
-		assertEquals("Elevator0",assignedElevator1.getElevatorID()); 
+		assertEquals(assignedElevator1.getElevatorID(), "Elevator0"); 
 		
 		// Bring Elevator0 to floor 2
 		try
@@ -66,111 +63,7 @@ public class SchedulerTest {
 		// of them in order of ID.
 		Elevator assignedElevator2 = scheduler.CallElevator(person2);
 		
-		assertEquals("Elevator1",assignedElevator2.getElevatorID());
-	}
-	
-	@Test
-	void scheduler_assign_elevators_with_more_people_added() {  
-	    Person person1 = new Person(0,3);
-		Person person2 = new Person(10,14);
-		Person person3 = new Person(2,4);
-		
-		
-		Elevator assignedElevator1 = scheduler.CallElevator(person1);
-		assertEquals("Elevator0", assignedElevator1.getElevatorID());
-
-		
-		try {
-			assignedElevator1.GoToFloor(3);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		Elevator assignedElevator2 = scheduler.CallElevator(person2);
-		
-		assertEquals("Elevator0", assignedElevator2.getElevatorID());
-		try
-		{
-			assignedElevator2.GoToFloor(14);
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		
-		assertFalse(assignedElevator1.isIdle());
-		assertFalse(assignedElevator2.isIdle());
-		assertTrue(assignedElevator2.getPeopleInside().isEmpty());
-		
-		
-		Elevator assignedElevator3 = scheduler.CallElevator(person3);
-		
-		assertEquals("Elevator1", assignedElevator3.getElevatorID());
-		try
-		{
-			assignedElevator3.GoToFloor(3);
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		assertFalse(assignedElevator3.isIdle());
-		assertFalse(assignedElevator3.getPeopleInside().isEmpty());
-	}
-	
-	@Test
-	void scheduler_assign_elevators_with_more_people_and_elevator_added() { 
-		Elevator elevator4 = new Elevator();
-		Elevator elevator5 = new Elevator();
-		Elevator elevator6 = new Elevator();
-		Elevator elevator7 = new Elevator();
-		
-		Person person1 = new Person(0,3);
-		Person person2 = new Person(10,14);
-		Person person3 = new Person(2,4);
-		Person person4 = new Person(0,3);
-		Person person5 = new Person(1,2);
-		Person person6 = new Person(3,4);
-		Person person7 = new Person(2,4);
-		
-		scheduler.AddElevator(elevator4);
-		scheduler.AddElevator(elevator5);
-		scheduler.AddElevator(elevator6);
-		scheduler.AddElevator(elevator7);
-		
-		Elevator assignedElevator1 = scheduler.CallElevator(person1);
-		assertEquals("Elevator0", assignedElevator1.getElevatorID());
-
-		
-		try {
-			assignedElevator1.GoToFloor(3);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		Elevator assignedElevator2 = scheduler.CallElevator(person2);
-		assertEquals("Elevator1", assignedElevator2.getElevatorID());
-		assertFalse(assignedElevator1.isIdle());
-		assertFalse(assignedElevator2.isIdle());
-		assertFalse(assignedElevator2.getPeopleInside().isEmpty());
-		
-		Elevator assignedElevator3 = scheduler.CallElevator(person3);
-		
-		assertFalse(assignedElevator3.isIdle());
-		assertFalse(assignedElevator3.getPeopleInside().isEmpty());
-		assertEquals("Elevator2", assignedElevator3.getElevatorID());
-		
-		
-		Elevator assignedElevator4 = scheduler.CallElevator(person4);
-		assertEquals("Elevator3", assignedElevator4.getElevatorID());
-
-		Elevator assignedElevator5 = scheduler.CallElevator(person5);
-		assertEquals("Elevator4", assignedElevator5.getElevatorID());
-		
-		Elevator assignedElevator6 = scheduler.CallElevator(person6);
-		assertEquals("Elevator5", assignedElevator6.getElevatorID());
-		
-		Elevator assignedElevator7 = scheduler.CallElevator(person7);
-		assertEquals("Elevator6", assignedElevator7.getElevatorID());
-		
+		assertEquals(assignedElevator2.getElevatorID(), "Elevator1");
 	}
 
 }
